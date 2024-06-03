bdb7fe7aa20afcbd0b133d1299cb5bdf8f696e16
Tabia Ye <tabiaye@gmail.com>
2024-06-02 20:59:18 -0700

    finished thunks

--- react-vite/src/redux/results.js
+++ react-vite/src/redux/results.js
@@ -37,4 +37,43 @@ export const thunkAddResult = (result) => async (dispatch) => {
     }
 }; 
 
+// can only update result within 15 minutes of the game being over
+export const thunkUpdateResult = (result, resultId) => async (dispatch) => {
+    const response = await fetch(`/api/results/${resultId}/update`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(result)
+    }); 
+    if (response.ok) {
+        const updatedResult = await response.json();
+        return dispatch(addResult(updatedResult));
+    } else {
+        return { server: 'Something went wrong. Please try again' }
+    }
+}; 
+
+
+const initialState = {}; 
+
+function resultsReducer(state = initialState, action) {
+    switch (action.type) {
+        case LOAD_RESULTS: {
+            const newState = { ...state };
+            action.results.forEach((result) => {
+                newState[result.id] = result;
+            });
+            return newState;
+        }
+        case ADD_RESULT: {
+            const newState = { ...state }; 
+            newState[action.result.id] = action.result;
+            return newState;
+        }
+        default: 
+            return state;
+    }
+}; 
+
+
+export default resultsReducer;
 
