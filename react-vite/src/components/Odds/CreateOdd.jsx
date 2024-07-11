7115f00bc7361bdde3e126efca4affaf1d869c71
Tabia Ye <tabiaye@gmail.com>
2024-07-11 13:44:28 -0700

    add odds css

--- react-vite/src/components/Odds/CreateOdd.jsx
+++ react-vite/src/components/Odds/CreateOdd.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'; 
-import { useParams } from 'react-router-dom'; 
+import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { thunkAddOdd } from '../../redux/games';
 
@@ -15,8 +15,8 @@ function AddOdd() {
     const currentGame = allGames?.find((game) => game.id == gameId); 
     const homeTeam = allTeams?.find((team) => team.id == currentGame.homeTeamId); 
     const awayTeam = allTeams?.find((team) => team.id == currentGame.awayTeamId); 
-    const formattedDate = currentGame.startTime.split(' ').slice(0, -2).join(' ');
-    const formattedTime = new Date(currentGame.startTime).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}); 
+    const formattedDate = currentGame?.startTime.split(' ').slice(0, -2).join(' ');
+    const formattedTime = new Date(currentGame?.startTime).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}); 
     const homeTeamOdds = allOdds?.filter((odd) => odd.gameId == currentGame.id && odd.teamId == currentGame.homeTeamId && odd.status == 'open'); 
     const homeSpread = homeTeamOdds?.find((odd) => odd.type == 'spread'); 
     const homeTotals = homeTeamOdds?.find((odd) => odd.type == 'totals');
@@ -29,34 +29,39 @@ function AddOdd() {
 
     return (
         <div id='create-odds-container'>
-            <div id='odds-game-container'>
-                <table className='game-table-header'>
-                    <tr>
-                        <th className='table-header' style={{width:'34%', textAlign:'left', color:'#f76900'}}>GAME</th>
-                        <th className='table-header' style={{width:'22%'}}>SPREAD</th>
-                        <th className='table-header' style={{width:'22%'}}>TOTAL</th>
-                        <th className='table-header' style={{width:'22%'}}>MONEYLINE</th>
-                    </tr>
-                </table>
-                <table className='game-table-body'>
-                    <tr>
-                        <td style={{width:'34%', fontSize:'12px'}}>{formattedDate}&nbsp;&nbsp;&mdash;&nbsp;&nbsp;{formattedTime}</td>
-                    </tr>
-                    <tr>
-                        <td style={{width:'34%', color: 'white'}}><div className='team-and-logo'><img className='team-logo' src={homeTeam.logo}/>{homeTeam.name}</div></td>
-                        <td className='data-field'>{homeSpread ? homeSpread.value : ''}</td>
-                        <td className='data-field'>{homeTotals ? homeTotals.value : ''}</td>
-                        <td className='data-field'>{homeMoneyline ? homeMoneyline.value : ''}</td>
-                    </tr>
-                    <tr>
-                        <td style={{width:'34%', color: 'white'}}><div className='team-and-logo'><img className='team-logo' src={awayTeam.logo}/>{awayTeam.name}</div></td>
-                        <td className='data-field'>{awaySpread ? awaySpread.value : ''}</td>
-                        <td className='data-field'>{awayTotals ? awayTotals.value : ''}</td>
-                        <td className='data-field'>{awayMoneyline ? awayMoneyline.value : ''}</td>
-                    </tr>
-                </table>
+            <div id='odds-back-btn-games-container'>
+                <div id='odds-back-to-games'>
+                    <Link className='back-btn' to='/games'>Go back to Games</Link>
+                </div>
+                <div id='odds-game-container'>
+                    <table className='game-table-header'>
+                        <tr>
+                            <th className='table-header' style={{width:'34%', textAlign:'left', color:'#f76900'}}>GAME</th>
+                            <th className='table-header' style={{width:'22%'}}>SPREAD</th>
+                            <th className='table-header' style={{width:'22%'}}>TOTAL</th>
+                            <th className='table-header' style={{width:'22%'}}>MONEYLINE</th>
+                        </tr>
+                    </table>
+                    <table className='game-table-body'>
+                        <tr>
+                            <td style={{width:'34%', fontSize:'12px'}}>{formattedDate}&nbsp;&nbsp;&mdash;&nbsp;&nbsp;{formattedTime}</td>
+                        </tr>
+                        <tr>
+                            <td style={{width:'34%', color: 'white'}}><div className='team-and-logo'><img className='team-logo' src={homeTeam?.logo}/>{homeTeam?.name}</div></td>
+                            <td className='data-field'>{homeSpread ? homeSpread.value : ''}</td>
+                            <td className='data-field'>{homeTotals ? homeTotals.value : ''}</td>
+                            <td className='data-field'>{homeMoneyline ? homeMoneyline.value : ''}</td>
+                        </tr>
+                        <tr>
+                            <td style={{width:'34%', color: 'white'}}><div className='team-and-logo'><img className='team-logo' src={awayTeam?.logo}/>{awayTeam?.name}</div></td>
+                            <td className='data-field'>{awaySpread ? awaySpread.value : ''}</td>
+                            <td className='data-field'>{awayTotals ? awayTotals.value : ''}</td>
+                            <td className='data-field'>{awayMoneyline ? awayMoneyline.value : ''}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-    </div>
+        </div>
     )
 
 }
