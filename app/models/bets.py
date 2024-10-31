977560cfbc4d0384e6760244533792eba871f542
Tabia Ye <tabiaye@gmail.com>
2024-10-31 13:48:34 -0700

    added amount to bets model

--- app/models/bets.py
+++ app/models/bets.py
@@ -10,6 +10,7 @@ class Bet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
     odd_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('odds.id')))
+    amount = db.Column(db.Integer, nullable=False)
     status = db. Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.Datetime, default=datetime.now())
@@ -22,5 +23,6 @@ class Bet(db.Model):
             'id': self.id, 
             'userId': self.user_id, 
             'oddId': self.odd_id, 
+            'amount': self.amount,
             'status': self.status
         }
